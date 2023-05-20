050312e40ba2edb4565ec95611d514adbd907b87
zzkk <118501002+zihui123@users.noreply.github.com>
2023-05-20 22:18:11 +0800

    Create Final.java

---
+++ Final_Project/Final.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.sql.*;
+
+public class final_2 {
+	public static void main(String[] args) {
+		
+		Login frame = new Login();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
