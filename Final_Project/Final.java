34b531f8b2bbaa02f8407fb93b22d17ccb677568
Jessie475 <118877744+Jessie475@users.noreply.github.com>
2023-05-23 11:41:58 +0800

    Add a system.out.....
    For testing

--- Final_Project/Final.java
+++ Final_Project/Final.java
@@ -4,6 +4,7 @@ import java.sql.*;
 public class final_2 {
 	public static void main(String[] args) {
 		
+		System.out.println("hi");
 		Login frame = new Login();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
