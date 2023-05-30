86e819486bd345fb8535422958cf258d84af7dea
Jessie475 <118877744+Jessie475@users.noreply.github.com>
2023-05-30 18:50:57 +0800

    sperrate applicants information from recruit table

--- Final_Project/src/My_status.java
+++ Final_Project/src/My_status.java
@@ -1,18 +1,22 @@
 import javax.swing.*;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Vector;
 
 public class My_status extends JFrame{
 	private Connection conn;
 	boolean sucess;
 	private JTable join;
 	private JTable recruit;
-	private JScrollPane scrollPane1;
-	private JScrollPane scrollPane2;
+	private JScrollPane joinScrollPane;
+	private JScrollPane recruitScrollPane;
 	ResultSet result;
 	JPanel panel = new JPanel();
 	public My_status(){
 		createTable();
-		createPanel();
 		setTitle("My Status");
 	}
 	public void createTable() {
@@ -26,41 +30,69 @@ public class My_status extends JFrame{
 			if (sucess) {
 			result = stat.getResultSet();
 			// 定義表格的數據
-			Object[][] data = {
-				{"", "", "", ""},
-				{"", "", "", ""},
-				{"", "", "", ""}
-			};
+			
 			// 創建一個新的JTable實例
 			join = new JTable(data, columnNames);
 			}
 
 			// 定義表格的標題
-			String[] columnNames2 = {"Course name", "Status of recruit", "Applied request", "Applicants' name", "Department", "Message from applicant"};
+			String[] columnNames2 = {"Course name", "Status of recruit", "Applied request"};
 			query = "";
 			sucess = stat.execute(query);
 			if(sucess){
 				result = stat.getResultSet();
 				// 定義表格的數據
-				Object[][] data2 = {
-					{"", "", ""},
-					{"", "", ""},
-					{"", "", ""}
-				};
+				
+				// 把數據加入表格
 				recruit = new JTable(data2, columnNames2);
-			}
+				}
+			
 			
 			// 為了能夠滾動查看所有數據，我們將表格放入一個JScrollPane。
-			scrollPane1 = new JScrollPane(join);
-			scrollPane2 = new JScrollPane(recruit);
-			result.close();			
-	} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
+			joinScrollPane = new JScrollPane(join);
+			recruitScrollPane = new JScrollPane(recruit);
+			result.close();	
+			
+			JLabel JoinLabel = new JLabel("Join");
+			JLabel RecruitLabel = new JLabel("Recruit");
 	
+			 // 使用BoxLayout佈局管理器
+			 BoxLayout boxLayout = new BoxLayout(getContentPane(), BoxLayout.Y_AXIS);
+			 setLayout(boxLayout);
+
+			  // 添加組件
+			  add(JoinLabel);
+			  add(joinScrollPane);
+			  add(RecruitLabel);
+			  add(recruitScrollPane);
+			} 
+		catch (SQLException e1) {
+			e1.printStackTrace();
+			}
+	
+		// 添加一個MouseListener到你的表格中
+	recruit.addMouseListener(new MouseAdapter() {
+    public void mouseClicked(MouseEvent e) {
+        if (e.getClickCount() == 2) { // 雙擊事件
+            JTable target = (JTable)e.getSource();
+            int row = target.getSelectedRow(); // 獲取被點擊的行的索引
+
+            // 獲取該行對應的申請者列表
+            
+
+            // 創建一個JDialog來展示申請者的資訊
+            JDialog dialog = new JDialog(My_status.this, "Aplicants' Information");
+            dialog.setSize(400, 300);
+
+            // 創建一個JList來展示申請者的姓名
+            JList<String> list = new JList<>();
+            dialog.add(new JScrollPane(list));
+
+            dialog.setVisible(true);
+        }
+    }
+});
+
 	}
-	public void createPanel(){
-		panel.add(scrollPane1, scrollPane2);
-		panel.setVisible(true);
-	}
+	
 }
